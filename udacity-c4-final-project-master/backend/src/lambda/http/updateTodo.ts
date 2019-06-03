30b67331e246cc76737182cf969de9c944c69594
Eddy Shyu <eddy@udacity.com>
2019-06-02 18:55:15 -0700

    added solution for project

---
+++ udacity-c4-final-project-master/backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,30 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { updateTodo } from '../../businessLogic/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    await updateTodo(getUserId(event), todoId, updatedTodo)
+
+    return {
+      statusCode: 200,
+      body: ''
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
